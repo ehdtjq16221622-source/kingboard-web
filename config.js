04fc60d3d15d9362008b7e60ac0cae32fc9df7fb
ehdtjq16221622 <ehdtjq16221622@gmail.com>
2026-03-16 18:49:43 +0900

    add supabase config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Supabase 설정 - 배포 전 반드시 입력
 const CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT_ID.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_KEY",
+  SUPABASE_URL: "https://luiukcbveioxujazvrct.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1aXVrY2J2ZWlveHVqYXp2cmN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NDcxMTgsImV4cCI6MjA4OTIyMzExOH0.OkXrMBbIRSRWuB4BN7mxHQ44hfGIraa-YRhZeLWTHQc",
 };
